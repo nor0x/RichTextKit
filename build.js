f39cbfc754e6baee6a62a93009580927db1238ef
Johnny <nor0x@users.noreply.github.com>
2020-06-24 21:22:55 +0200

    build js updated

--- build.js
+++ build.js
@@ -1,40 +1,27 @@
-var bt = require('./buildtools/buildTools.js')
+var bt = require("./buildtools/buildTools.js");
 
-if (bt.options.official)
-{
-    // Check everything committed
-    bt.git_check();
+if (bt.options.official) {
+  // Check everything committed
+  bt.git_check();
 
-    // Clock version
-    bt.clock_version();
+  // Clock version
+  bt.clock_version();
 
-    // Clean build directory
-    bt.run("rm -rf ./Build");
+  // Clean build directory
+  bt.run("rm -rf ./Build");
 }
 
 // Build
-bt.run("dotnet build Topten.RichTextKit -c Release")
+bt.run("dotnet build Topten.RichTextKit -c Release");
 
-if (bt.options.official)
-{
-    // Run tests
-    bt.run("dotnet test Topten.RichTextKit.Test -c Release");
+if (bt.options.official) {
+  // Tag and commit
+  bt.git_tag();
 
-    // Build docs
-    if (!bt.options.nodoc)
-    {
-        bt.run(`docsanity`);
-        bt.run(`git add doc`);
-        bt.run(`git commit --allow-empty -m "Updated documentation"`);
-    }
-
-    // Tag and commit
-    bt.git_tag();
-
-    // Push nuget package
-    bt.run(`dotnet nuget push`,
-           `./Build/Release/*.${bt.options.version.build}.nupkg`,
-           `--source nuget.org`);
+  // Push nuget package
+  bt.run(
+    `dotnet nuget push`,
+    `./Build/Release/*.${bt.options.version.build}.nupkg`,
+    `--source nuget.org`
+  );
 }
-
-
